da7400254da4f7f0ab1b875e61191834753852f0
Guilherme Tavares <zetavares.rib@gmail.com>
2020-05-07 16:00:38 -0300

    rmv daterange

--- src/django_plus/apps.py
+++ src/django_plus/apps.py
@@ -1,14 +1,9 @@
 from django.apps import AppConfig
 from suit.apps import DjangoSuitConfig
-from rangefilter.apps import RangeFilterConfig
 
 
 class SuitConfig(DjangoSuitConfig):
     layout = "horizontal"
-
-
-class DateRangeConfig(RangeFilterConfig):
-    pass
 
 
 class DjangoPlusConfig(AppConfig):
