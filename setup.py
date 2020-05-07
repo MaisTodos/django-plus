fe202e42e0101dc493c714d69642174f608a9261
Guilherme Tavares <zetavares.rib@gmail.com>
2020-05-07 10:35:24 -0300

    add develop

--- setup.py
+++ setup.py
@@ -18,9 +18,13 @@ setup_requires = parse_requirements(
     os.path.join(ROOT, 'requirements', 'base.txt'), session='hack')
 setup_requires = [str(ir.req) for ir in setup_requires]
 
+print(setup_requires)
+
 tests_requires = [
     'pytest==3.0.7',
 ]
+
+setup_requires = []
 
 setup(
     name="cielo",
